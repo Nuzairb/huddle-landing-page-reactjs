8d9d9b65b6dd8af1563f7a61cab54db38140c3a2
Nuzair Ibrahim <nuzairb@gmail.com>
2019-01-07 22:16:27 +0500

    init

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/core/Menu';
+import IconButton from '@material-ui/core/IconButton';
+
 export default class Header extends Component {
   render() {
     return (
       <div>
-        Header
+        <AppBar 
+          position="fixed"
+          style={{
+            backgroundColor: '#2f2f2f',
+            boxShadow: 'none',
+            padding: '10px 0px'
+          }}
+        >
+          Header
+        </AppBar>
       </div>
     )
   }
