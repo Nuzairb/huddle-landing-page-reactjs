1a9683795003a3d0e245ac79b91f902b2f19fd40
Nuzair Ibrahim <nuzairb@gmail.com>
2019-01-13 20:37:32 +0500

    init

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carrousel from './Carrousel';
+import TimeUntil from './TimeUntil';
 
 const Featured = () => {
     return (
@@ -12,7 +13,7 @@ const Featured = () => {
                     Ariana Grande
                 </div>
             </div>
-
+            <TimeUntil/>
         </div>
     );
 };
